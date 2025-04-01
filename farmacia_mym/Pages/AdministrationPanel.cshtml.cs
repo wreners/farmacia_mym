acf9189c30e53eb0a485b4f0144373a88e5ed887
Edwin Patzan <112340158+Edwin5886@users.noreply.github.com>
2025-04-01 15:49:47 -0600

    implementacion de panel

---
+++ farmacia_mym/Pages/AdministrationPanel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace farmacia_mym.Pages
+{
+    public class AdministrationPanelModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
